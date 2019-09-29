ac535a96ebe25bcf608aab3ea27a278ee2351931
sirkubax <muszynski@so1.ai>
2019-09-29 18:39:51 +0200

    hosts create

--- opt/create_hosts.sh
+++ opt/create_hosts.sh
@@ -1,6 +1,7 @@
 ansible-playbook playbooks/create_aws_instance.yml -e "type=t2.nano hostname=apache02 role=frontend"
 ansible-playbook playbooks/create_aws_instance.yml -e "type=t2.nano hostname=apache01 role=frontend"
 ansible-playbook playbooks/create_aws_instance.yml -e "type=t2.nano hostname=backend01 role=backend"
+
 #ansible-playbook playbooks/create_aws_instance.yml -e "type=t2.nano hostname=wodrpress01_szk16 role=wordpress"
 #ansible-playbook playbooks/create_aws_instance.yml -e "type=t2.nano hostname=wodrpress01_szk01 role=wordpress"
 #ansible-playbook playbooks/create_aws_instance.yml -e "type=t2.nano hostname=wodrpress01_szk02 role=wordpress"
@@ -31,4 +32,8 @@ ansible-playbook playbooks/create_aws_instance.yml -e "type=t2.nano hostname=bac
 #ansible-playbook playbooks/create_aws_instance.yml -e "type=t2.nano hostname=wodrpress02_szk04 role=wordpress"
 #ansible-playbook playbooks/create_aws_instance.yml -e "type=t2.nano hostname=wodrpress02_szk05 role=wordpress"
 #
-for i in `seq -w 01 17`; do ansible-playbook playbooks/create_aws_instance.yml -e "type=t2.nano hostname=mysql01_szk$i role=wordpress"; done
+#for i in `seq -w 01 17`; do ansible-playbook playbooks/create_aws_instance.yml -e "type=t2.nano hostname=wordpress01_szk$i role=wordpress"; done
+#for i in `seq -w 01 17`; do ansible-playbook playbooks/create_aws_instance.yml -e "type=t2.nano hostname=mysql01_szk$i role=mysql"; done
+#for i in `seq -w 01 17`; do ansible-playbook playbooks/create_aws_instance.yml -e "type=t2.nano hostname=loadbalancer01_szk$i role=loadbalancer"; done
+#for i in `seq -w 01 17`; do ansible-playbook playbooks/create_aws_instance.yml -e "type=t2.nano hostname=wordpress02_szk$i role=wordpress"; done
+#ansible-playbook playbooks/create_aws_instance.yml -e "type=t2.large hostname=awx role=awx"
